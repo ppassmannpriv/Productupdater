2ab0b9a11987b5ce61fb81c04c48fe4904e8f1b0
Pieter Paßmann <pieter.passmann@gmail.com>
2015-08-06 20:27:31 +0200

    for now have a composer 'vendor' directory along side the lib directory. we should try to change this to lib inside of composer though so parsecsv.lib.php is nicely integrated and updateable

--- app/code/local/Scriptkid/Productupdater/Helper/Csv.php
+++ app/code/local/Scriptkid/Productupdater/Helper/Csv.php
@@ -5,5 +5,18 @@ class Scriptkid_Productupdater_Helper_Csv extends Mage_Core_Helper_Abstract {
     /* get that one really good CSV Interpreter from github
     *  and never credit it ...
     */
-    include('../../../../../../vendor/parsecsv/php-parsecsv/parsecsv.lib.php');
+    private $csv = false;
+
+    public function __construct()
+    {
+      require_once Mage::getBaseDir().DS.'vendor/parsecsv/php-parsecsv/parsecsv.lib.php';
+      $this->csv = new parseCSV();
+    }
+
+    public function initParser($file, $encoding, $delimiter)
+    {
+      $this->csv->encoding($encoding);
+      $this->csv->delimiter($delimiter);
+      return $this->csv->parse($file);
+    }
 }
