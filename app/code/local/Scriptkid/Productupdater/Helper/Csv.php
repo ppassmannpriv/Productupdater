6463a213e3afe1190f0d6a4d2500e91c6218d6c6
Pieter Paßmann <pieter.passmann@gmail.com>
2015-08-06 08:42:08 +0200

    trying to put vendor stuff over composer into magento extensions - funtimes

--- app/code/local/Scriptkid/Productupdater/Helper/Csv.php
+++ app/code/local/Scriptkid/Productupdater/Helper/Csv.php
@@ -5,4 +5,5 @@ class Scriptkid_Productupdater_Helper_Csv extends Mage_Core_Helper_Abstract {
     /* get that one really good CSV Interpreter from github
     *  and never credit it ...
     */
+    include('../../../../../../vendor/parsecsv/php-parsecsv/parsecsv.lib.php');
 }
